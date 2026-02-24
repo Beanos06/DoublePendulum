da5f544ec3b183e89a6a5f43281dbea37abecbaf
Steven Li <stevenliyul@gmail.com>
2026-02-24 09:16:47 -0500

    Integrated rk4 to update method of DoublePendulum class

--- src/rk4.py
+++ src/rk4.py
@@ -1,7 +1,7 @@
-def rk4_step(f, y, t, dt):
-    r1 = f(t, y)
-    r2 = f(t + dt/2, y + dt/2 * r1)
-    r3 = f(t + dt/2, y + dt/2 * r2)
-    r4 = f(t + dt, y + dt * r3)
+def rk4_step(f, y, t, dt, *args):
+    r1 = f(t, y, *args)
+    r2 = f(t + dt/2, y + dt/2 * r1, *args)
+    r3 = f(t + dt/2, y + dt/2 * r2, *args)
+    r4 = f(t + dt, y + dt * r3, *args)
 
-    next_step = y + (r1 + 2*r2 + 2*r3 + r4) * dt / 6
+    return y + (r1 + 2*r2 + 2*r3 + r4) * dt / 6
